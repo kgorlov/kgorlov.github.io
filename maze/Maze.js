84d3b6e8890138440439a3756917d337539c4bcd
kgorlov <kirillgorlov90@gmail.com>
2018-05-04 19:25:40 +0300

    лабиринт готово!

--- maze/Maze.js
+++ maze/Maze.js
@@ -34,6 +34,11 @@ function drawMaze(mazeFile, startinX, startinY) {
         context.drawImage(imgMaze, 0, 0)
 
         x = startinX;
+        canvas.height = imgMaze.height;
+
+        context.drawImage(imgMaze, 0, 0)
+
+        x = startinX;
         y = startinY;
 
         var imgFace = document.getElementById("face")
@@ -49,61 +54,66 @@ function drawMaze(mazeFile, startinX, startinY) {
 function processKey(e) {
     dx = 0;
     dy = 0;
+
+    context.beginPath();
+    context.fillStyle = "rgb(254, 244, 207)";
+    context.rect(x, y, 15, 15);
+    context.fill();
+
     if (e.keyCode == 38) {
-        dy = -1;
+        dy = -3;
     }
-    if(e.keyCode == 40) {
-        dy = 1;
+    if (e.keyCode == 40) {
+        dy = 3;
     }
 
     if (e.keyCode == 37) {
-        dx = -1;
+        dx = -3;
     }
 
     if (e.keyCode == 39) {
-        dx = 1;
+        dx = 3;
     }
 }
 
 function redraw() {
-    if (dx != 0 || dy != 0){
-        context.beginPath();
-        context.fillStyle = "rgb(254, 244, 207)";
-        context.rect(x, y, 15, 15);
-        context.fill();
-
+    if (dx != 0 || dy != 0) {
         x += dx;
         y += dy;
 
-        if (checkCollision() ) {
-            x-= dy;
-            y-= dy;
-            dy = 0;
+        if (checkCollision()) {
+            x -= dx;
+            y -= dy;
             dx = 0;
+            dy = 0;
         }
 
         var imgFace = document.getElementById("face");
         context.drawImage(face, x, y);
 
     }
+    if (y > 575) {
+        alert("Ты победил");
+        y = 5;
+    }
     timer = setTimeout("redraw()", 10);
 }
 
 function checkCollision() {
-    var imgData = context.getImageData(x-1, y-1, 15+2, 15+2);
+    var imgData = context.getImageData(x - 1, y - 1, 15 + 2, 15 + 2);
     var pixels = imgData.data;
 
-    for(var i = 0; n=pixels.length, i<n; i+=4) {
+    for (var i = 0; n = pixels.length, i < n; i += 4) {
         var red = pixels[i];
-        var green = pixels[i+1];
-        var blue = pixels[i+2];
+        var green = pixels[i + 1];
+        var blue = pixels[i + 2];
 
-        if (red==0 && green==0 && blue==0) {
+        if (red == 0 && green == 0 && blue == 0) {
             return true;
         }
-        if (red==169 && green==169 && blue==169) {
+        if (red == 169 && green == 169 && blue == 169) {
             return true;
         }
-    } 
+    }
     return false;
 }
